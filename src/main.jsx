5c1ccc4ba48428ece4b17afabad12c403b6181d6
Ahmad Musa <mycontribusion01@gmail.com>
2026-05-22 19:45:19 +0100

    remain chrome, analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,5 +10,6 @@ registerSW({ immediate: true })
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
